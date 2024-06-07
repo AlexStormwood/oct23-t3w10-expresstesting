bae03610e8baa6a0137b1f20d766e3fdb8fb7152
Alex <alex.holder@bigfootds.com>
2024-06-08 09:41:44 +1000

    feat: error handling middleware in place

--- src/server.js
+++ src/server.js
@@ -7,9 +7,19 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/", (request, response, next) => {
+
+	return next(new Error("Error on purpose from root route"))
+
 	response.json({
 		message:"Hello world!"
+	});
+});
+
+app.use((error, request, response, next) => {
+	response.json({
+		message:"Error occured!",
+		error: error
 	});
 });
 
